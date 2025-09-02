01e19a18df27ce5e90bc7d662e8932e3997ee14c
Jack <jack@agilitee.com>
2025-09-02 10:39:21 -0400

    CKYE-24: Add close button to AddUserModal

--- src/components/organisms/AddUserModal/AddUserModal.jsx
+++ src/components/organisms/AddUserModal/AddUserModal.jsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState } from 'react';
+import Image from 'next/image';
 import TextField from '@/components/atoms/TextField/TextField';
 import Dropdown from '@/components/atoms/Dropdown/Dropdown';
 import Button from '@/components/atoms/Button/Button';
@@ -37,6 +38,18 @@ const AddUserModal = ({ closeModal, workspaces = [], addUsers, workspace = null 
         <div className={styles['add-user-modal__content']}>
           <div className={styles['add-user-modal__header']}>
             <h2 className={styles['add-user-modal__title']}>Add User</h2>
+            <button 
+              className={styles['add-user-modal__close']}
+              onClick={closeModal}
+              aria-label="Close modal"
+            >
+              <Image
+                src="/cross.svg"
+                alt="Close"
+                width={16}
+                height={16}
+              />
+            </button>
           </div>
           
           <div className={styles['add-user-modal__divider']} />
